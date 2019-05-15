c74f7993d58d88d6a7a9094cbba84f0b2ff1940d
sjz <123@123>
2019-05-15 09:49:31 +0800

    second commit

---
+++ JZAppHelper/Tools/YYHelper.h
@@ -0,0 +1,17 @@
+//
+//  YYHelper.h
+//  JZAppHelper
+//
+//  Created by sunjz on 2019/5/15.
+//  Copyright © 2019 yiyangwang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YYHelper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
--- JZAppHelper/Tools/YYHelper.m
+++ JZAppHelper/Tools/YYHelper.m
@@ -1,15 +1,13 @@
 //
-//  JZHelperHeader.h
+//  YYHelper.m
 //  JZAppHelper
 //
 //  Created by sunjz on 2019/5/15.
 //  Copyright © 2019 yiyangwang. All rights reserved.
 //
 
-#ifndef JZHelperHeader_h
-#define JZHelperHeader_h
-
 #import "YYHelper.h"
 
+@implementation YYHelper
 
-#endif /* JZHelperHeader_h */
+@end
